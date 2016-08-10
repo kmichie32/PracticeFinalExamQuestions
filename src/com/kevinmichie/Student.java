5829d4de8034a3ab6616561326282f1f9fd486c2
Kevin Michie <kmichie32@gmail.com>
2016-08-10 09:39:12 -0500

    Fixed Hashmap

--- src/com/kevinmichie/Student.java
+++ src/com/kevinmichie/Student.java
@@ -1,6 +1,7 @@
 package com.kevinmichie;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -9,16 +10,30 @@ import java.util.Map;
 public class Student {
 
     private String id;
+    private ArrayList<String> students;
 
     public Student(String studentId) {
         this.id = studentId;
+    }
+
+    public void addToStudents(String studentName) {
+        students.add(studentName);
+    }
+
+    public void removeStudents(String studentName) {
+        students.remove(studentName);
     }
 
     public String getId() {
         return id;
     }
 
-   /* public Map<String,Student> indexStudents(String id, ArrayList<Student>) {
-
-    }*/
+    public Map<String,Student> indexStudents(String id, ArrayList<Student> students) {
+        Student student = new Student(id);
+        Map studentsOrganized = new HashMap<String,Student>();
+        for(Student person: students) {
+            studentsOrganized.put(student.getId(), person);
+        }
+        return studentsOrganized;
+    }
 }
